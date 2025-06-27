db8f059e8bd46e4110ecf1d62032d81a6ddb039b
Styl Bandeira <stylbandeira@gmail.com>
2025-06-27 08:12:33 -0300

    Welcome Tests

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,12 +1,32 @@
 import { Link, Head } from '@inertiajs/react';
 import MainLayout from '@/Layouts/MainLayout';
+import ApplicationLogo from '@/Components/ApplicationLogo';
+import { FaUser } from 'react-icons/fa';
 
 export default function Welcome(props) {
     return (
         <>
-            <h1>
-                Welcome
-            </h1>
+            <div className="flex justify-content-center">
+                <a href="/" rel="noopener noreferrer">
+                    <img className="object-contain h-32" src="/storage/img/primary-logo.png" alt="Logo" />
+                </a>
+            </div>
+            <div className="mail">
+                <div className="mail-card">
+                    <h1>Olá, Fulano</h1>
+                    <p>Clique no botão abaixo para redefinir sua senha:</p>
+                    <a href='/'
+                        className="btn btn-success">
+                        Redefinir Senha
+                    </a>
+
+                    <hr />
+
+                    <div className="align-self-middle">
+                        <p>Se você não solicitou isso, ignore este e-mail</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
