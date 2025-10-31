9bbf63543ef7e74021f8edb622d8f51c7c757e69
Styl Bandeira <stylbandeira@gmail.com>
2025-10-31 20:55:25 -0300

    Creates users resource for admin

---
+++ app/Http/Resources/AdminUserResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Log;
+
+class AdminUserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        Log::info(['user' => $this]);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'type' => $this->type,
+            // 'img' => $this->img ? config('app.url') . '/storage/' . $this->img : null,
+            'email' => $this->email,
+            'points' => $this->points,
+            'reputation' => $this->reputation,
+            'cpf' => $this->cpf,
+            'status' => $this->status,
+
+            //Only for Admin
+            'email_verified_at' => $this->email_verified_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+        ];
+    }
+}
