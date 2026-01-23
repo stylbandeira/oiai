8cd4279fac0dee3e389dd687abe4c9af94aa70a4
Styl Bandeira <stylbandeira@gmail.com>
2026-01-23 10:52:10 -0300

    Create Invoice Model

---
+++ app/Models/Invoice.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+    protected $table = 'invoice';
+
+    public $fillable = [
+        'access_key',
+        'user_id',
+        'company_id',
+        'receipt_data'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
