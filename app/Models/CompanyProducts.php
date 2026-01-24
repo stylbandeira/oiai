d59bf6bc9f6e1162750ff94d682c913742f002a9
Styl Bandeira <stylbandeira@gmail.com>
2026-01-24 20:38:59 -0300

    Fillable

--- app/Models/CompanyProducts.php
+++ app/Models/CompanyProducts.php
@@ -10,6 +10,12 @@ class CompanyProducts extends Model
     use HasFactory;
     protected $table = 'company_products';
 
+    protected $fillable = [
+        'product_id',
+        'company_id',
+        'average_price'
+    ];
+
     public function company()
     {
         return $this->belongsTo(Company::class, 'company_id');
