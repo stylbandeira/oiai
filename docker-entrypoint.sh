f09b1f7ac8a16118d281a31d538a2392920aec24
Styl Bandeira <stylbandeira@gmail.com>
2026-01-28 18:07:52 -0300

    Coloca worker queue em background

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -28,5 +28,9 @@ php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
 
+echo "Iniciando Queue Worker em background..."
+# Worker rodando EM BACKGROUND (não controlado pelo Supervisor)
+php artisan queue:work --daemon --sleep=3 --tries=3 --timeout=60 > /var/www/html/storage/logs/worker.log 2>&1 &
+
 echo "Tudo pronto! Iniciando Supervisor..."
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
