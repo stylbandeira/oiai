667d00e0344fcab836fb9b5e99ca9e792a24c34e
Styl Bandeira <stylbandeira@gmail.com>
2026-01-28 21:11:44 -0300

    Create notification service

---
+++ app/Services/NotificationService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class NotificationService
+{
+
+    /**
+     * TODO - Criar notificação para um usuário
+     *
+     * @return void
+     */
+    public function sendUserNotification(): void {}
+}
