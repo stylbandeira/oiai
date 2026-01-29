da60782accf7f5b377c7e9d1749e3430a18fda43
Styl Bandeira <stylbandeira@gmail.com>
2026-01-28 21:10:38 -0300

    Register event observer

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Event;
 use App\Models\User;
+use App\Observers\EventObserver;
 use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -29,6 +31,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         User::observe(UserObserver::class);
+        Event::observe(EventObserver::class);
     }
 
     /**
