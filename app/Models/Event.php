031672b5c09effe538465612dc81c2d19e916c82
Styl Bandeira <stylbandeira@gmail.com>
2026-01-28 21:09:41 -0300

    Create event model

---
+++ app/Models/Event.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+    protected $table = 'event';
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'where',
+        'type',
+        'points',
+        'link',
+    ];
+}
