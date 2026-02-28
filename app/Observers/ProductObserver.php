01d2c29e4bd9812aac65b7dcdec6f5a3d679a02c
Styl Bandeira <stylbandeira@gmail.com>
2026-02-28 16:54:21 -0300

    Add points when user is validating

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -3,11 +3,30 @@
 namespace App\Observers;
 
 use App\Models\Product;
+use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Auth;
 
 class ProductObserver
 {
+    protected $user;
+    protected $userRepo;
+
+    public function __construct(UserRepository $userRepo)
+    {
+        $this->user = Auth::user();
+        $this->userRepo = $userRepo;
+    }
     public function creating(Product $product)
     {
         $product->mentioned_quantity++;
+        $product->created_by = $this->user->id;
+    }
+
+    public function updating(Product $product)
+    {
+        if ($product->getOriginal('validated') === false && !$product->validated_by) {
+            $this->userRepo = UserRepository::class;
+            $this->userRepo->addPoints($product->created_by, 3);
+        }
     }
 }
