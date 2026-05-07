2e2fb1bfc476468849eb37d5dac9daea94ab0750
Styl Bandeira <stylbandeira@gmail.com>
2026-05-07 18:46:08 -0300

    create Ownership Request Event

--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -52,8 +52,36 @@ class EventRepository
             'title' => Event::TYPE_PRODUCT_INSERT,
             'description' => ucwords(strtolower($user->name)) . ' adicionou ' . $quantity . ' produtos.',
             'where' => $company->name ?? '',
-            'type' => 'product_insert',
+            'type' => 'client',
             'points' => $quantity,
+            'link' => $this->generateLink()
+        ];
+
+        try {
+            $this->model->create($event);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Create a event from when an user requests an access to an company account.
+     *
+     * @param User $user
+     * @param Company $company
+     * @return void
+     */
+    public function createOwnershipRequestEvent(User $user, Company $company)
+    {
+        $event = [];
+
+        $event = [
+            'user_id' => $user->id,
+            'title' => Event::TYPE_COMPANY_OWNER_REQUEST,
+            'description' => ucwords(strtolower($user->name)) . ' solicitou acesso de admin à empresa: ' . $company->name,
+            'where' => $company->name ?? '',
+            'type' => 'admin',
+            'points' => 0,
             'link' => $this->generateLink()
         ];
 
