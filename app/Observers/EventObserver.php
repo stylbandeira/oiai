2a67848f05e89814a0d400831c66c6fb1f33c339
Styl Bandeira <stylbandeira@gmail.com>
2026-05-07 18:45:55 -0300

    Remove EventObserver functions

--- app/Observers/EventObserver.php
+++ app/Observers/EventObserver.php
@@ -5,27 +5,4 @@ namespace App\Observers;
 use App\Models\Event;
 use App\Services\NotificationService;
 
-class EventObserver
-{
-    protected $notification;
-
-    public function __construct(NotificationService $notification)
-    {
-        $this->notification = $notification;
-    }
-
-    public function created(Event $event)
-    {
-        switch ($event->type) {
-            case 'product_insert':
-                $user = $event->user;
-                $user->has_notification = true;
-                $user->save();
-                break;
-
-            default:
-                # code...
-                break;
-        }
-    }
-}
+class EventObserver {}
