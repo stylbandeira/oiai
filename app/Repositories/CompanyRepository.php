9c2c1af71a42c060096143a23bab19c5afe18d25
Styl Bandeira <stylbandeira@gmail.com>
2026-06-05 20:23:16 -0300

    feat: returns only its own relationship with the compan

--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -59,6 +59,13 @@ class CompanyRepository
             $query->where('status', $request->status);
         }
 
+        if ($request->user()->type === 'company') {
+            $query->with('ownerRelationship')
+                ->whereHas('ownerRelationship', function ($query) use ($request) {
+                    $query->where('user_id', $request->user()->id);
+                });
+        }
+
         if ($request->user()->type === 'admin') {
 
             //TO-DO - QUERO DEIXAR ISSO MAIS AUTOMÁTICO
