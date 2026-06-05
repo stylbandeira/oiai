7b646095c6d3aca29ece0906345d481b49d4d74d
Styl Bandeira <stylbandeira@gmail.com>
2026-06-05 20:24:04 -0300

    test: create unity factory

---
+++ database/factories/UnityFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Unity>
+ */
+class UnityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'abbreviation' => fake()->word(),
+            'name' => fake()->word(),
+        ];
+    }
+}
