28bc1357243d4754d7a33d9c8a15c1aaaea32c0c
Styl Bandeira <stylbandeira@gmail.com>
2026-06-05 11:55:16 -0300

    feat: remove comma

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'country' => fake()->country(),
+            'area' => fake()->name(),
+            'number' => fake()->numberBetween(),
+            'complement' => Str::random(1),
+            'latitude' => fake()->latitude(),
+            'longitude' => fake()->longitude(),
+            'state' => Str::random(2),
+        ];
+    }
+}
