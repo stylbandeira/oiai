0c853808c82c149ead116ae14df5c8ed06d32945
Styl Bandeira <stylbandeira@gmail.com>
2026-06-10 18:58:11 -0300

    feat: remove MustVerifyEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use App\Notifications\ResetPasswordNotification;
 use App\Notifications\VerifyEmailNotification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
