788dae3e84849f52472e64d40e7e575d38003f9e
Styl Bandeira <stylbandeira@gmail.com>
2026-06-15 11:08:49 -0300

    feat: creates and uses requests for ListItens

--- app/Http/Controllers/ListItensController.php
+++ app/Http/Controllers/ListItensController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ListItens\ListItensUpdateRequest;
 use App\Models\ItensList;
 use App\Models\ListProducts;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
 
 class ListItensController extends Controller
 {
@@ -14,30 +12,19 @@ class ListItensController extends Controller
      * Update list itens
      *
      * @param ItensList $list
-     * @param Request $request
+     * @param ListItensUpdateRequest $request
      * @return void
      */
-    public function update(Request $request, ItensList $list)
+    public function update(ListItensUpdateRequest $request, ItensList $list)
     {
         $list->load('listProducts');
-
-        $validator = Validator::make($request->all(), [
-            'completed_items' => 'required|array',
-            'completed_items.*' => 'integer|exists:products,id'
-        ]);
-
-        if ($validator->fails()) {
-            return response([
-                'errors' => $validator->errors()
-            ], 422);
-        }
 
         try {
             $list->listProducts()->update([
                 'completed' => false
             ]);
 
-            $completedItems = $validator->validated()['completed_items'];
+            $completedItems = $request->validated()['completed_items'];
 
             if (!empty($completedItems)) {
                 $listItems = ListProducts::where('list_id', $list->id)
