a1be093f958fc4a2d88a537cdf920310f5c23fe4
Styl Bandeira <stylbandeira@gmail.com>
2026-06-15 11:06:22 -0300

    feat: creates and uses requests for Events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,55 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Event\EventCheckAllRequest;
+use App\Http\Requests\Event\EventUpdateRequest;
 use App\Models\Event;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
 
 class EventController extends Controller
 {
-    public function update(Request $request, Event $event)
+    public function update(EventUpdateRequest $request, Event $event)
     {
-        $validator = Validator::make($request->all(), [
-            'user_id' => 'nullable|integer',
-            'title' => 'string',
-            'description' => 'string',
-            'where' => 'nullable|string',
-            'type' => 'string',
-            'points' => 'integer',
-            'link' => 'string',
-            'checked' => 'boolean',
-            'target_type' => 'string',
-            'entity_type' => 'nullable|string',
-            'entity_id' => 'nullable|integer',
-        ]);
-
-        if ($validator->fails()) {
-            return response([
-                'errors' => $validator->errors()
-            ], 422);
-        }
-
-        $event->update($validator->validated());
+        $event->update($request->validated());
 
         return response(['message' => 'Evento alterado com sucesso!']);
     }
 
-    public function checkAll(Request $request)
+    public function checkAll(EventCheckAllRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'notifications' => 'required|array',
-            'notifications.*.id' => 'required|integer',
-        ]);
-
-        if ($validator->fails()) {
-            return response([
-                'errors' => $validator->errors()
-            ], 422);
-        }
-
-        $notifications_ids = collect($validator->validated()['notifications'])
+        $notifications_ids = collect($request->validated()['notifications'])
             ->pluck('id');
 
         try {
