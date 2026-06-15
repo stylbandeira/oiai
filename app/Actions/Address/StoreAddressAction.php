2eb6659ea2940fb4b2f53abdb474cd79eda85d29
Styl Bandeira <stylbandeira@gmail.com>
2026-06-15 12:26:31 -0300

    feat: Address Repository

--- app/Actions/Address/StoreAddressAction.php
+++ app/Actions/Address/StoreAddressAction.php
@@ -4,12 +4,20 @@ namespace App\Actions\Address;
 
 use App\Http\Requests\Address\AddressStoreRequest;
 use App\Models\Address;
+use App\Repositories\AddressRepository;
 
 class StoreAddressAction
 {
+    private AddressRepository $addressRepository;
+
+    public function __construct(AddressRepository $addressRepository)
+    {
+        $this->addressRepository = $addressRepository;
+    }
+
     public function execute(AddressStoreRequest $request)
     {
-        $address = Address::create($request->validated());
+        $address = $this->addressRepository->create($request->validated());
 
         return response([
             'address' => $address,
