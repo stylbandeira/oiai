4366ac69d353dc1cac51e8afcd197e227999a9b1
Styl Bandeira <stylbandeira@gmail.com>
2026-06-17 15:10:58 -0300

    feat: creates and uses ProductCategory Repository

--- app/Actions/ProductCategory/IndexProductCategoryAction.php
+++ app/Actions/ProductCategory/IndexProductCategoryAction.php
@@ -3,12 +3,16 @@
 namespace App\Actions\ProductCategory;
 
 use App\Http\Resources\CategoryResource;
-use App\Models\ProductCategory;
+use App\Repositories\ProductCategoryRepository;
 
 class IndexProductCategoryAction
 {
+    public function __construct(
+        private ProductCategoryRepository $productCategoryRepository,
+    ) {}
+
     public function execute()
     {
-        return CategoryResource::collection(ProductCategory::all());
+        return CategoryResource::collection($this->productCategoryRepository->all());
     }
 }
