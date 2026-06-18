10faf163faa774503f7c4d3401dc0a231554197a
Styl Bandeira <stylbandeira@gmail.com>
2026-06-18 14:27:49 -0300

    feat: refactor action and controller using SOLID

--- app/Actions/Unity/IndexUnityAction.php
+++ app/Actions/Unity/IndexUnityAction.php
@@ -2,29 +2,17 @@
 
 namespace App\Actions\Unity;
 
-use App\Http\Requests\Unity\UnityIndexRequest;
-use App\Http\Resources\UnityResource;
-use App\Models\Unity;
+use App\Repositories\UnityRepository;
 
 class IndexUnityAction
 {
-    public function execute(UnityIndexRequest $request)
+    public function __construct(
+        private UnityRepository $unity_repository,
+    ) {}
+
+    public function execute(array $array)
     {
-        $query = Unity::query();
-
-        if ($request->filled('search')) {
-            $searchTerm = '%' . $request->search . '%';
-
-            $query->where(function ($query) use ($searchTerm) {
-                $query->where('name', 'like', $searchTerm)
-                    ->orWhere('abbreviation', 'like', $searchTerm);
-            });
-        }
-
-        $unities = $query
-            ->orderBy('name')
-            ->paginate($request->per_page ?? 15);
-
-        return UnityResource::collection($unities);
+        $unities = $this->unity_repository->paginate($array);
+        return $unities;
     }
 }
