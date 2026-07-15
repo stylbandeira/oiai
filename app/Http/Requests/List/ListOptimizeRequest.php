03265637286ded6798137edbffa92c63fdadbd3c
Styl Bandeira <stylbandeira@gmail.com>
2026-07-15 15:39:44 -0300

    feat: create ListOptimizeRequest

---
+++ app/Http/Requests/List/ListOptimizeRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\List;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ListOptimizeRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'latitude' => [
+                'sometimes',
+                'numeric',
+                'between:-90,90',
+                'required_with:longitude',
+            ],
+            'longitude' => [
+                'sometimes',
+                'numeric',
+                'between:-180,180',
+                'required_with:latitude',
+            ],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(response([
+            'message' => 'Erro ao tentar otimizar lista',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}
