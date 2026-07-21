7ea48ab0bc2f46e93fc5a4914d5e3fbb70d139e8
Styl Bandeira <stylbandeira@gmail.com>
2026-07-21 10:25:39 -0300

    feat: create and transfer functions to InvoiceService

--- app/Services/Invoice/InvoiceService.php
+++ app/Services/Invoice/InvoiceService.php
@@ -6,6 +6,10 @@ use App\Models\Invoice;
 
 class InvoiceService
 {
+    public const NFE_MODEL = '55';
+
+    public const NFCE_MODEL = '65';
+
     public function isValid(string $invoiceCode): bool
     {
         return $this->areaIsValid($invoiceCode);
@@ -13,6 +17,22 @@ class InvoiceService
 
     public function areaIsValid(string $invoiceCode): bool
     {
-        return in_array(substr($invoiceCode, 0, 2), array_values(Invoice::VALID_AREA_CODES));
+        return in_array(substr($this->normalizeAccessKey($invoiceCode), 0, 2), array_values(Invoice::VALID_AREA_CODES), true);
     }
+
+    public function normalizeAccessKey(string $invoiceCode): string
+    {
+        return preg_replace('/\D/', '', $invoiceCode) ?? '';
+    }
+
+    public function documentModel(string $invoiceCode): string
+    {
+        return substr($this->normalizeAccessKey($invoiceCode), 20, 2);
+    }
+
+    public function isNFCe(string $invoiceCode): bool
+    {
+        return $this->documentModel($invoiceCode) === self::NFCE_MODEL;
+    }
+
 }
