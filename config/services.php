8892ddd295afd4586ffbcee91c90653ee8dbb113
Styl Bandeira <stylbandeira@gmail.com>
2026-08-04 09:52:29 -0300

    config: configure cosmos ans oscbr data

--- config/services.php
+++ config/services.php
@@ -26,6 +26,25 @@ return [
         'url' => env('NOMINATIM_URL', 'https://nominatim.openstreetmap.org/search')
     ],
 
+    'cosmos' => [
+        'token' => env('COSMOS_API_TOKEN'),
+        'url' => env('COSMOS_API_URL', 'https://api.cosmos.bluesoft.com.br'),
+        'user_agent' => env('COSMOS_API_USER_AGENT', 'Cosmos-API-Request'),
+        'batch_size' => env('COSMOS_API_DAILY_PRODUCT_COUNT', 7),
+        'recurrence_minutes' => env('COSMOS_API_RECURRENCE_MINUTES', 180),
+        'daily_limit' => env('COSMOS_API_DAILY_LIMIT', 45),
+    ],
+
+    'oscbr' => [
+        'login' => env('OSCBR_API_LOGIN'),
+        'password' => env('OSCBR_API_PASSWORD'),
+        'auth_url' => env('OSCBR_AUTH_URL', 'https://gtin.rscsistemas.com.br/api/v3/oauth/token'),
+        'product_url' => env('OSCBR_API_URL', 'https://gtin.rscsistemas.com.br/api/v3/gtin'),
+        'batch_size' => env('OSCBR_API_DAILY_PRODUCT_COUNT', 5),
+        'recurrence_minutes' => env('OSCBR_API_RECURRENCE_MINUTES', 5),
+        'daily_limit' => env('OSCBR_API_DAILY_LIMIT', 50),
+    ],
+
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
